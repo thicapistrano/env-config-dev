c720108511cecc9dd83aa31293341ad874a833c1
thicapistrano <thiago.capistrano@gmail.com>
2021-09-27 13:49:15 -0300

    insert github cli

--- init.sh
+++ init.sh
@@ -67,3 +67,15 @@ echo 'finished install docker-compose'
 echo 'isntall tree'
 sudo snap tree
 echo 'finished install tree'
+
+echo 'install github cli'
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install gh -y
+echo 'finished install github cli'
+
+echo 'finished'
+sudo apt update
+sudo apt upgrade
+echo 'command final'
